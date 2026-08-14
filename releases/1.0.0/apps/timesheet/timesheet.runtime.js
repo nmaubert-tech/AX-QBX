d93cc6a1ffd83342bdcbb8ac64ec17ec123b3914
nmaubert-tech <nmaubert@gmail.com>
2026-08-13 21:24:42 -0600

    Fix Home navigation and hide diagnostics behind support shortcut

--- releases/1.0.0/apps/timesheet/timesheet.runtime.js
+++ releases/1.0.0/apps/timesheet/timesheet.runtime.js
@@ -1,84 +1,63 @@
 (function (global) {
     "use strict";
 
-    function getLogger() {
-        return global.AxialQB && global.AxialQB.logger ? global.AxialQB.logger : null;
-    }
-
-    function getApplication() {
-        return global.AxialTimesheet || null;
-    }
-
-    function configureLoggerCompatibility() {
-        var logger = getLogger();
-        if (logger && !logger.getEntries && logger.getRecords) {
-            logger.getEntries = logger.getRecords;
-        }
-    }
+    function getLogger() { return global.AxialQB && global.AxialQB.logger ? global.AxialQB.logger : null; }
+    function getApplication() { return global.AxialTimesheet || null; }
+    function configureLoggerCompatibility() { var logger = getLogger(); if (logger && !logger.getEntries && logger.getRecords) { logger.getEntries = logger.getRecords; } }
 
     function downloadDiagnostics() {
         var logger = getLogger();
-        if (logger) {
-            logger.download("timesheet-diagnostics.json");
-        }
+        if (logger && typeof logger.download === "function") { logger.download("timesheet-diagnostics.json"); }
     }
 
     function clearDiagnostics() {
         var logger = getLogger();
-        if (logger) {
-            logger.clear();
-        }
-        if (global.jQuery) {
-            global.jQuery("#diagnosticsContent").text("");
-            global.jQuery("#diagnosticsButton").hide();
-        }
+        if (logger && typeof logger.clear === "function") { logger.clear(); }
+        if (global.jQuery) { global.jQuery("#diagnosticsContent").text(""); global.jQuery("#diagnosticsButton").hide(); }
     }
 
-    function closeDiagnostics() {
-        if (global.jQuery) {
-            global.jQuery("#diagnosticsPanel").hide();
-        }
+    function closeDiagnostics() { if (global.jQuery) { global.jQuery("#diagnosticsPanel").hide(); } }
+
+    function applicationDbid() {
+        var parts=(global.location.pathname||"").split("/").filter(function(x){return !!x;});
+        var dbIndex=parts.indexOf("db");
+        return dbIndex>=0&&parts[dbIndex+1]?parts[dbIndex+1]:"";
     }
 
     function goHome() {
-        if (global.aliasMap && global.aliasMap.app_id) {
-            global.location = global.aliasMap.app_id;
-            return;
+        var pageConfig = global.AXIAL_PAGE_CONFIG || {};
+        var env = global.AXIAL_TIMESHEET_ENV || {};
+        var target = pageConfig.homeUrl || env.homeUrl || "";
+        if (!target) {
+            var dbid = applicationDbid();
+            target = dbid ? "/db/" + encodeURIComponent(dbid) : "/";
         }
-        global.history.back();
+        global.location.href = target;
     }
 
     function bindWrapperControls() {
         if (!global.jQuery) { return; }
         var $ = global.jQuery;
-
-        $("#downloadDiagnosticsButton")
-            .off("click.axialRuntime")
-            .on("click.axialRuntime", downloadDiagnostics);
-
-        $("#clearDiagnosticsButton")
-            .off("click.axialRuntime")
-            .on("click.axialRuntime", clearDiagnostics);
-
-        $("#closeDiagnosticsButton")
-            .off("click.axialRuntime")
-            .on("click.axialRuntime", closeDiagnostics);
-
-        $("#homeButton")
-            .off("click.axialRuntime")
-            .on("click.axialRuntime", goHome);
+        $("#downloadDiagnosticsButton").off("click.axialRuntime").on("click.axialRuntime", downloadDiagnostics);
+        $("#clearDiagnosticsButton").off("click.axialRuntime").on("click.axialRuntime", clearDiagnostics);
+        $("#closeDiagnosticsButton").off("click.axialRuntime").on("click.axialRuntime", closeDiagnostics);
+        $("#homeButton").off("click.axialRuntime").on("click.axialRuntime", goHome);
     }
 
     configureLoggerCompatibility();
     bindWrapperControls();
 
+    global.addEventListener("keydown", function (event) {
+        if (event.ctrlKey && event.altKey && String(event.key || "").toLowerCase() === "d") {
+            event.preventDefault();
+            downloadDiagnostics();
+        }
+    });
+
     global.addEventListener("beforeunload", function (event) {
         var app = getApplication();
         if (!app || !global.AxialTimesheetView) { return; }
-        if (global.AxialTimesheetView.countDirty(app.state) > 0) {
-            event.preventDefault();
-            event.returnValue = "";
-        }
+        if (global.AxialTimesheetView.countDirty(app.state) > 0) { event.preventDefault(); event.returnValue = ""; }
     });
 
     global.downloadTimesheetDiagnostics = downloadDiagnostics;
